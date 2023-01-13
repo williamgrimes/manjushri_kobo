6fdb8f06eac07cbdcc9d94804a77bd010344009a
williamgrimes <williamgrimes@gmx.com>
2023-01-13 17:25:27 +0100

    Release with org-writer and anki-csv

--- core/argparser.py
+++ core/argparser.py
@@ -1,5 +1,8 @@
 """Argument paser for project"""
 import argparse
+import os
+
+from datetime import datetime
 
 
 def argparser():
@@ -13,8 +16,8 @@ def argparser():
                         default="logs/",
                         type=str,
                         help="folder containing logs.")
-    parser.add_argument("--min_word_len",
-                        default=3,
+    parser.add_argument("--max_word_len",
+                        default=2,
                         type=int,
                         help="number of words to differentiate a quote.")
     parser.add_argument("--sql_books_read",
@@ -25,4 +28,16 @@ def argparser():
                         default="sql/extract_annotations.sql",
                         type=str,
                         help="path to sql query returning annotations.")
+    parser.add_argument("--org_file",
+                        type=str,
+                        help="path to output book annotations..")
+    parser.add_argument("--org_user",
+                        default=os.environ.get('USER'),
+                        help="Name to add to header of org file.")
+    parser.add_argument("--org_date",
+                        default=datetime.now().strftime("%Y-%m-%d"),
+                        help="Name to add to header of org file.")
+    parser.add_argument("--org_initial_visibility",
+                        default="fold",
+                        help="Visibility of org-file.")
     return parser.parse_args()
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 """Extract annotations, words and phrases from Kobo Ereader."""
 import string
+from datetime import datetime
 
 import pandas as pd
-from wiktionaryparser import WiktionaryParser
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
 
 from core import argparser
 from core.database import KoboDB
@@ -10,39 +12,79 @@ from core.logs import ProjectLogger
 
 logger = ProjectLogger(__name__)
 
-wp = WiktionaryParser()
+
+def df_split_words_quotes(df: pd.DataFrame, max_word_len: int,**kwargs) -> (pd.DataFrame, pd.DataFrame):
+    df["word_count"] = df["Text"].str.split().str.len()
+    df_words, df_quotes = df[(mask := df['word_count'] <= max_word_len)].copy(), df[~mask].copy()
+    logger.i(f"Split df {len(df)} into df_words {len(df_words)} and quotes {len(df_quotes)}.")
+    return df_words, df_quotes
+
+def write_quotes(df_quotes, org_file, org_user, org_date, org_initial_visibility, **kwargs):
+    df_quotes.sort_values(by=["DateLastRead"], inplace=True)
+    replacers = {"\n": " ", "\r": " ", "\\xa0\\xa0\\xa0": " ", "  ": " "}
+    df_quotes["Text"] = df_quotes["Text"].str.strip()
+    df_quotes["Text"] = df_quotes["Text"].replace(replacers, regex=True)
+    group_cols = ["DateLastRead", "Title", "Attribution"]
+    df_grp = df_quotes.groupby(group_cols)
+    with open(org_file, 'w') as f:
+        f.write(f"#+TITLE: Book Annotations")
+        f.write(f"\n#+AUTHOR: {org_user}")
+        f.write(f"\n#+DATE: {org_date}")
+        f.write(f"\n#+STARTUP: {org_initial_visibility}\n")
+        for group_name, df_group in df_grp:
+            group_info = dict(zip(group_cols, group_name))
+            date = datetime.fromisoformat(group_info.get('DateLastRead')).strftime("%Y-%m:%d")
+            f.write(f"\n* {group_info.get('Title')}")
+            f.write(f"\n:PROPERTIES:")
+            f.write(f"\n:AUTHOR {group_info.get('Attribution')}")
+            f.write(f"\n:DATE LAST READ: {date}")
+            f.write(f"\n:END:")
+
+            for index, row in df_group.iterrows():
+                f.write(f"\n + /\"{row.Text}/\"\n")
+    return None
 
 
-def clean_words_string(words_string: str) -> str:
-    """Clean strings to words without punctuation."""
+def clean_word(words_string: str) -> str:
+    """Clean words, remove punctuation, make lowercase, and Lemmatize to."""
     words_string = words_string.lower()
 
     # Create a translation table to remove punctuation
     translator = str.maketrans('', '', string.punctuation)
-
     words_string = words_string.translate(translator)
 
-    words_string = words_string.lower()
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_word = lemmatizer.lemmatize(words_string)
 
-    return words_string
+    word = lemmatized_word.strip()
 
+    return word
 
-def word_wiktionary(wp: WiktionaryParser,
-                    word: str,
-                    keep_keys: list = ["definitions", "etymology"]) -> dict:
-    """Return wiktionary entry for word."""
-    word_dict = wp.fetch(word).pop()
+def define_words(df_words):
+    pos_code_map = {
+        "n": "Noun",
+        "v": "Verb",
+        "a": "Adjective",
+        "s": "Adjective Satellite",
+        "r": "Adverb"
+    }
 
-    try:
-        definitions = word_dict.get("definitions").pop()
-        etymology = word_dict.get("etymology")
-    except IndexError:
-        print(f"Could not define {word}")
+    cols = ["Definition", "PartOfSpeech", "Examples"]
+    def define(row):
+        try:
+            w = wordnet.synsets(row["Text"]).pop()
+            return pd.Series(dict(zip(cols, [w.definition(), pos_code_map.get(w.pos()), w.examples()])))
+        except IndexError:
+            return pd.Series(dict(zip(cols, [None, None, None])))
 
-    # TODO determine return format for parts of speech
+    df_words[cols] = df_words.apply(define, axis=1)
 
-    return word_dict
+    df_undefined, df_defined = df_words[(mask := df_words["Definition"].isna())].copy(), df_words[~mask].copy()
 
+    undefined_words = list(df_undefined["Text"])
+    for word in undefined_words:
+        logger.i(f"Defintion not found in wordnet: \"{word}\"")
+    return df_defined
 
 if __name__ == '__main__':
     args = argparser.argparser()
@@ -51,18 +93,24 @@ if __name__ == '__main__':
     with KoboDB(kwargs.get("kobo_db")) as conn:
         cursor = conn.cursor()
         #df_books = KoboDB.run_query(cursor, kwargs.get("sql_books_read"))
-
         df = KoboDB.run_query(cursor, kwargs.get("sql_extract_annotations"))
-        df["word_count"] = df["Text"].str.split().str.len()
 
-        words = [clean_words_string(s) for s in list(
-            df["Text"]) if len(s.split()) < kwargs.get("min_word_len")]
+        df_words, df_quotes = df_split_words_quotes(df, **kwargs)
 
-        df_quotes = df.loc[df["Text"].str.split().apply(
-            len) > kwargs.get("min_word_len")]
+        write_quotes(df_quotes, **kwargs)
 
-        meanings = [word_wiktionary(wp, w) for w in words]
+        df_words["Text"] = df_words["Text"].apply(clean_word)
 
-        words_dict = dict(zip(words, meanings))
+        df_words = define_words(df_words)
 
-        df = pd.DataFrame(words_dict)
+        df_words[['Text', 'Definition']] = df_words[['Text', 'Definition']].apply(lambda x: x.str.capitalize())
+
+        df_words = df_words[['Text', 'Definition', "PartOfSpeech", "Examples"]]
+        _n = "\n"
+        df_words['Answer'] = df_words.apply(lambda x: f'<em>{x["Definition"]}\n\n\n</em><b>Part of speech:</b>\n<em>{x["PartOfSpeech"]}</em>\n\n<b>Examples:</b>\n<em>{f"{_n}".join(x["Examples"])}</em>', axis=1)
+
+        df_words['Text'] = df_words['Text'].apply(lambda x: f"<b>{x}</b>")
+        df_words = df_words.drop_duplicates(subset=['Text'])
+
+
+        df_words[['Text', 'Answer']].to_csv("test.csv", index=False, header=False)
