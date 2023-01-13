617f20eb8608fb2adf5bc66ab271646ec75814cd
williamgrimes <williamgrimes@gmx.com>
2023-01-13 17:24:16 +0100

    Comment out unused fields

--- sql/extract_annotations.sql
+++ sql/extract_annotations.sql
@@ -1,13 +1,14 @@
 SELECT
 -- Bookmark.VolumeID,
 Bookmark.Text,
-Bookmark.Annotation,
-Bookmark.ExtraAnnotationData,
+--Bookmark.Annotation,
+--Bookmark.ExtraAnnotationData,
 Bookmark.DateCreated,
 Bookmark.DateModified,
-content.BookTitle,
+--content.BookTitle,
 content.Title,
-content.Attribution
+content.Attribution,
+content.DateLastRead
 FROM Bookmark
-         INNER JOIN content
-                    ON Bookmark.VolumeID = content.ContentID;
+INNER JOIN content
+ON Bookmark.VolumeID = content.ContentID;
