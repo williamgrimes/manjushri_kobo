7fe04edbe40c89db28829138841393b10d01bdba
williamgrimes <williamgrimes@gmx.com>
2023-01-16 13:23:57 +0100

    Wordnet allow multiple definitions - improve formatting

--- main.py
+++ main.py
@@ -71,20 +71,49 @@ def define_words(df_words):
 
     cols = ["Definition", "PartOfSpeech", "Examples"]
     def define(row):
-        try:
-            w = wordnet.synsets(row["Text"]).pop()
-            return pd.Series(dict(zip(cols, [w.definition(), pos_code_map.get(w.pos()), w.examples()])))
-        except IndexError:
-            return pd.Series(dict(zip(cols, [None, None, None])))
+        word = row["Text"]
+        synsets = wordnet.synsets(word)
+        if synsets:
+            logger.i(f"\"{word}\" - {len(synsets)} definition(s) found.")
+            return [{"Definition": s.definition().capitalize(),
+                     "Part of speech": pos_code_map.get(s.pos()),
+                     "Examples": [e.capitalize() for e in s.examples()],
+                     } for s in synsets]
+        else:
+            logger.i(f"\"{word}\" - definition not found.")
+            return []
 
-    df_words[cols] = df_words.apply(define, axis=1)
+    df_words["Answer"] = df_words.apply(define, axis=1)
 
-    df_undefined, df_defined = df_words[(mask := df_words["Definition"].isna())].copy(), df_words[~mask].copy()
+    df_defined, df_undefined = df_words[(mask := df_words["Answer"].astype(bool))].copy(), df_words[~mask].copy()
 
     undefined_words = list(df_undefined["Text"])
     for word in undefined_words:
         logger.i(f"Defintion not found in wordnet: \"{word}\"")
     return df_defined
+
+def words_to_anki_csv(df_words):
+    df_words["Text"] = df_words["Text"].apply(lambda r: f"<h1>{r.capitalize()}</h1>")
+
+    def anki_format(answer):
+        lines_separator = "----------------------------------------"
+        def _format(i, answer):
+            answer_str = f"\n{lines_separator}"
+            answer_str += f"<large> {i + 1} </large>"
+            answer_str += f"{lines_separator}\n"
+            answer_str += f"<em><b>\n{answer['Definition']}</b> - ({answer['Part of speech']})</em>"
+            if examples := answer.get("Examples"):
+                answer_str += "<small>"
+                answer_str += f'<u>\n\nExamples:</u>\n'
+                for example in examples:
+                    answer_str += f"<em>\"{example}\"</em>\n"
+                answer_str += "</small>"
+            return answer_str
+        answer_str = [_format(i, a) for i, a in enumerate(answer)]
+        return f"\n".join(answer_str) + f"\n{lines_separator}---{lines_separator}"
+
+    df_words["Answer"] = df_words["Answer"].apply(anki_format)
+    return df_words
 
 if __name__ == '__main__':
     args = argparser.argparser()
@@ -101,16 +130,12 @@ if __name__ == '__main__':
 
         df_words["Text"] = df_words["Text"].apply(clean_word)
 
+        removed_rows = df_words[df_words.duplicated(subset=["Text"], keep=False)]
+        logger.i(f"Duplicates dropped for: {list(removed_rows['Text'])}")
+        df_words = df_words.drop_duplicates(subset=["Text"], keep='first')
+
         df_words = define_words(df_words)
 
-        df_words[['Text', 'Definition']] = df_words[['Text', 'Definition']].apply(lambda x: x.str.capitalize())
-
-        df_words = df_words[['Text', 'Definition', "PartOfSpeech", "Examples"]]
-        _n = "\n"
-        df_words['Answer'] = df_words.apply(lambda x: f'<em>{x["Definition"]}\n\n\n</em><b>Part of speech:</b>\n<em>{x["PartOfSpeech"]}</em>\n\n<b>Examples:</b>\n<em>{f"{_n}".join(x["Examples"])}</em>', axis=1)
-
-        df_words['Text'] = df_words['Text'].apply(lambda x: f"<b>{x}</b>")
-        df_words = df_words.drop_duplicates(subset=['Text'])
-
+        df_words = words_to_anki_csv(df_words)
 
         df_words[['Text', 'Answer']].to_csv("test.csv", index=False, header=False)
